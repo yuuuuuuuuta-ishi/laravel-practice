827b57d33bb21aaf65c1b7656a97c91137ac9526
next-ishihara-yuta <y.ishihara@nexttrail.co.jp>
2023-08-01 18:51:01 +0900

    環境修正

--- app/sample/app/Http/Middleware/EncryptCookies.php
+++ app/sample/app/Http/Middleware/EncryptCookies.php
@@ -6,12 +6,12 @@ use Illuminate\Cookie\Middleware\EncryptCookies as Middleware;
 
 class EncryptCookies extends Middleware
 {
-    // /**
-    //  * The names of the cookies that should not be encrypted.
-    //  *
-    //  * @var array<int, string>
-    //  */
-    // protected $except = [
-    //     'XDEBUG_SESSION'
-    // ];
+    /**
+     * The names of the cookies that should not be encrypted.
+     *
+     * @var array<int, string>
+     */
+    protected $except = [
+        //
+    ];
 }
