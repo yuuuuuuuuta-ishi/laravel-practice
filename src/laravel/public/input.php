91f836200d959537ab0203fc3d33411c237555d7
next-ishihara-yuta <140572246+next-ishihara-yuta@users.noreply.github.com>
2024-03-15 12:18:06 +0900

    add answer example

---
+++ src/laravel/public/input.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Input html</title>
+</head>
+<body>
+	<p>文字を入力してください。</p>
+	<form method="post" action="Output.php">
+		<input type="text" name="text">
+		<input type="submit" value="実行">
+	</form>
+</body>
+</html>
