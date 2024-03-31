61760ab9f5e30cc882ae076be7d71376a9edeaff
next-ishihara-yuta <140572246+next-ishihara-yuta@users.noreply.github.com>
2024-03-31 13:38:56 +0900

    add

--- src/laravel/app/Http/Controllers/SampleController.php
+++ src/laravel/app/Http/Controllers/SampleController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Session;
+
 
 class SampleController extends Controller
 {
@@ -14,6 +18,21 @@ class SampleController extends Controller
     public function output(request $request)
     {
         $text = $request->input('text');
-        return view('output', ['text' => $text]);
+        $response = Http::asForm()->post('https://api.a3rt.recruit.co.jp/talk/v1/smalltalk', [
+            'apikey' => env('A3RT_APY_KEY'),
+            'query' => $text,
+        ]);
+        Log::debug('message', $response->collect()->toArray());
+        Log::debug($response->json());
+
+
+        $deepL = Http::withHeaders(['Authorization' => 'DeepL-Auth-Key ceaa4860-7c6a-45c3-bbce-769302559ab4:fx', 'Content-Type' => 'application/json'])->asForm()->post('https://api-free.deepl.com/v2/translate', [
+            'text' => $text,
+            'target_lang' => "ja",
+        ]);
+        Log::debug('deepL', $deepL->collect()->toArray());
+        Log::debug($deepL->json());
+        return view('output', ['text' => json_encode($response->json()), 'deepL' => json_encode($deepL->json())]);
     }
+
 }
