cf405e4b7c23b5b5ac08aa103026c220d47ec57a
next-sato-akira <a.sato@nexttrail.co.jp>
2024-06-26 18:00:47 +0900

    Add:出力画面を作成
    生年月日、干支、星座を受け取り、表示する

---
+++ src/laravel/resources/views/problem3/output.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>problem3-output</title>
+    </head>
+    <body>
+        <h1>Controller基礎</h1>
+        <hr>
+        <h2>
+            「{{ $birthdayArray["year"] }}年{{ $birthdayArray["month"] }}月{{ $birthdayArray["day"] }}日生まれの方は{{ $eto }}年の{{ $seiza }}座です。」
+        </h2>
+    </body>
+</html>
