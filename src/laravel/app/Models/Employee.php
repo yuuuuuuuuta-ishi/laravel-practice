0e32cda84f82a9e813c7a186c182aadca5a0976d
next-sato-akira <a.sato@nexttrail.co.jp>
2024-06-28 17:22:38 +0900

    Add:設定と問題5で使用する関数を追加
    社員コードとパスワードからデータを取得する関数を追加

--- src/laravel/app/Models/Employee.php
+++ src/laravel/app/Models/Employee.php
@@ -8,4 +8,41 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'password',
+        'name',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'id',
+        'password',
+    ];
+
+    /**
+     * 社員コードとパスワードから一致するデータを取得する
+     * 
+     * @param string $code
+     * @param string $password
+     * 
+     * @return Employee $employee | null
+     */
+    public function findByCodeAndPassword($code, $password)
+    {
+        $employee = $this::where('employee_code', $code)->where('password', hash('sha1', $password))->first();
+        return $employee;
+    }
 }
